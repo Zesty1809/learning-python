319143f1b52fdad76892e33cee52a363a180649a
zesty <adi8116315284@gmail.com>
2025-10-23 00:49:36 +0530

    Day 1: Strings Part A

--- week1/day1basics.py
+++ week1/day1basics.py
@@ -43,3 +43,54 @@ print(message.upper())
 message = "Hello, World!"
 
 print(message.lower())
+
+
+# string.count() method
+# Program 9: How many spaces are there?
+sentence = "Python is fun to learn"
+
+print(sentence.count(' '))
+
+
+# Program 10: Count the number of 'l'
+message = "Hello World"
+
+print(message.count('l'))
+
+
+# Program 11: How many 5's are there?
+data = "12345-67890"
+
+print(data.count('5'))
+
+
+# Program 12: How many times 'na' appear?
+text = "banana"
+print(text.count('na'))
+
+
+# string.replace() method
+# Program 13: What's the output?
+text = "hello world"
+result = text.replace('world', 'python')
+print(result)
+# <-- OUTPUT -->
+# hello python
+
+
+# Program 14: Concatenate strings
+
+greeting = "Hello"
+name = "Michael"
+
+message = greeting + ', ' + name + '. Welcome!'
+
+print(message)
+
+
+# Program 15: Formatted strings
+
+greeting = "Hello"
+name = "Michael"
+
+message = '{}, {}. Welcome!'.format(greeting, name)
