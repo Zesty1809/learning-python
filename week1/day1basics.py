0c35f1da9cb2aebd4437050490d0cfb6631b645b
zesty <adi8116315284@gmail.com>
2025-10-23 00:13:56 +0530

    Initial commit

---
+++ week1/day1basics.py
@@ -0,0 +1,45 @@
+# Program 1: Print welcome message
+print('Hello World')
+
+
+# Program 2: Print welcome message using variables
+message = "Hello, World!"
+
+print(message)
+
+
+# Program 3: Print length of a string
+message = "Hello, World!"
+
+print(len(message))
+
+
+# Program 4: Accessing the first character of the string
+message = "Hello, World!"
+
+print(message[0])
+
+
+# Slicing
+# Program 5: Printing just the first word
+message = "Hello, World!"
+
+print(message[:5])
+
+
+# Program 6: Printing just the last word
+message = "Hello, World"
+
+print(message[7:])
+
+
+# Program 7: Convert the string to uppercase
+message = "Hello, World!"
+
+print(message.upper())
+
+
+# Program 8: Convert the string to lowercase
+message = "Hello, World!"
+
+print(message.lower())
